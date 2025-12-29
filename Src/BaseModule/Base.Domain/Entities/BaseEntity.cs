5f02d420c1d291d4afaf336ba5d1c5a3b9ef2595
Rafiei-Tolid <rafiei.alireza@yahoo.com>
2025-12-29 17:11:47 +0330

    add base entity

---
+++ Src/BaseModule/Base.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace NGErp.Base.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+       
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual DateTime CreateAt { get; set; } = DateTime.UtcNow;
+        public string? TimeZone { get; set; }
+        public int? UserCreationId { get; set; }
+        [DefaultValue(false)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public bool IsDeleted { get; set; }
+        public virtual DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public int? UpdatedUserId { get; set; }
+
+        public void MapBase(EntityTypeBuilder builder)
+        {
+            //builder.HasKey("Id");
+            builder.Property<bool>("IsDeleted").HasColumnType("bit").HasDefaultValueSql("0");
+            builder.Property<DateTime>("CreateAt").HasColumnType("datetime2").HasDefaultValueSql("GETUTCDATE()");
+            builder.Property<DateTime>("UpdatedAt").HasColumnType("datetime2").HasDefaultValueSql("GETUTCDATE()");
+            builder.Property<string?>("TimeZone").HasColumnType("nvarchar(50)");
+            builder.Property<int?>("UserCreationId").HasColumnType("int");
+            builder.Property<int?>("UpdatedUserId").HasColumnType("int");
+            builder.HasIndex("IsDeleted");
+        }
+
+    }
+}
---
+++ Src/BaseModule/Base.Domain/Entities/EntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NGErp.Base.Domain.Entities
+{
+    public interface IBaseEntityTypeConfiguration<T> where T : BaseEntity
+    {
+        public void Map(EntityTypeBuilder<T> builder);
+    }
+    public interface IViewModelTypeConfiguration<T> where T : class
+    {
+        public void Map(EntityTypeBuilder<T> builder);
+    }
+
+}
