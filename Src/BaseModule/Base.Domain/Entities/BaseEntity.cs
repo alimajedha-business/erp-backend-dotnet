db7a84a3994f2869c43874c7031da568f3b5e798
Rafiei-Tolid <rafiei.alireza@yahoo.com>
2025-12-30 16:36:15 +0330

    add BaseEntity add DbContext add Department entity

--- Src/BaseModule/Base.Domain/Entities/BaseEntity.cs
+++ Src/BaseModule/Base.Domain/Entities/BaseEntity.cs
@@ -10,26 +10,34 @@ namespace NGErp.Base.Domain.Entities
 {
     public abstract class BaseEntity
     {
-       
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }       
+
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public virtual DateTime CreateAt { get; set; } = DateTime.UtcNow;
+
         public string? TimeZone { get; set; }
-        public int? UserCreationId { get; set; }
+
+        public Guid? CreatorId { get; set; }
+
         [DefaultValue(false)]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public bool IsDeleted { get; set; }
+
         public virtual DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-        public int? UpdatedUserId { get; set; }
+
+        public Guid? ModifierId { get; set; }
 
         public void MapBase(EntityTypeBuilder builder)
         {
-            //builder.HasKey("Id");
+            builder.HasKey("Id");
+            builder.Property("Id").HasDefaultValueSql("NEWID()");
             builder.Property<bool>("IsDeleted").HasColumnType("bit").HasDefaultValueSql("0");
             builder.Property<DateTime>("CreateAt").HasColumnType("datetime2").HasDefaultValueSql("GETUTCDATE()");
             builder.Property<DateTime>("UpdatedAt").HasColumnType("datetime2").HasDefaultValueSql("GETUTCDATE()");
             builder.Property<string?>("TimeZone").HasColumnType("nvarchar(50)");
-            builder.Property<int?>("UserCreationId").HasColumnType("int");
-            builder.Property<int?>("UpdatedUserId").HasColumnType("int");
+            builder.Property<Guid?>("CreatorId").HasColumnType("uniqueidentifier");
+            builder.Property<Guid?>("ModifierId").HasColumnType("uniqueidentifier");
             builder.HasIndex("IsDeleted");
         }
 
---
+++ Src/HCMModule/HCM.Domain/HCM.Domain/Entities/Department.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NGErp.Base.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGErp.HCM.Domain.Entities
+{
+    public class Department : BaseEntity, IBaseEntityTypeConfiguration<Department>
+    {
+        public string? Code { get; set; }
+
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public bool Status { get; set; } = true;
+
+        public DateTime StatusChangeDate { get; set; }
+
+        public void Map(EntityTypeBuilder<Department> builder)
+        {
+            builder.ToTable("Department", "HCM");
+            builder.Property(e => e.Name).HasMaxLength(100);
+            builder.Property(e => e.Description).HasMaxLength(500);
+        }
+    }
+}
