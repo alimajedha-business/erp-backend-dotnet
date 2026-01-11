6b37a3096492b55d576f5ed4acfdb00d1db4341d
Ali Majed HA <ali.majedha.business@gmail.com>
2026-01-11 12:31:07 +0330

    add base-entity to general module

---
+++ Src/GeneralModule/General.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NGErp.General.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public virtual DateTime CreateAt { get; set; } = DateTime.UtcNow;
+
+    public string TimeZone { get; set; } = default!;
+
+    public Guid? CreatorId { get; set; }
+
+    [DefaultValue(false)]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public bool IsDeleted { get; set; }
+
+    public virtual DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    public Guid? ModifierId { get; set; }
+
+    public static void MapBase(EntityTypeBuilder builder)
+    {
+        builder
+            .HasKey("Id");
+
+        builder
+            .Property(nameof(Id))
+            .HasDefaultValueSql("NEWID()");
+
+        builder
+            .Property<bool>(nameof(IsDeleted))
+            .HasColumnType("bit")
+            .HasDefaultValueSql("0");
+
+        builder
+            .Property<DateTime>(nameof(CreateAt))
+            .HasColumnType("datetime2")
+            .HasDefaultValueSql("GETUTCDATE()");
+
+        builder
+            .Property<DateTime>(nameof(UpdatedAt))
+            .HasColumnType("datetime2")
+            .HasDefaultValueSql("GETUTCDATE()");
+
+        builder
+            .Property<string?>(nameof(TimeZone))
+            .HasColumnType("nvarchar(50)");
+
+        builder
+            .Property<Guid?>(nameof(CreatorId))
+            .HasColumnType("uniqueidentifier");
+
+        builder
+            .Property<Guid?>(nameof(ModifierId))
+            .HasColumnType("uniqueidentifier");
+
+        builder
+            .HasIndex("IsDeleted");
+    }
+
+}
