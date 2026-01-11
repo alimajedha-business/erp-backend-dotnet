36b6da3d5e74a32f6f89ba94c8a32e1bce823da0
Ali Majed HA <ali.majedha.business@gmail.com>
2026-01-11 13:00:20 +0330

    add entity-type config to general module domain

---
+++ Src/GeneralModule/General.Domain/EntityTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using NGErp.Base.Domain.Entities;
+
+namespace NGErp.General.Domain;
+
+internal class EntityTypeConfiguration
+{
+    public interface IBaseEntityTypeConfiguration<T> where T : BaseEntity
+    {
+        public void Map(EntityTypeBuilder<T> builder);
+    }
+
+    public interface IViewModelTypeConfiguration<T> where T : class
+    {
+        public void Map(EntityTypeBuilder<T> builder);
+    }
+}
