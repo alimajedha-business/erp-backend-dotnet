a6edbe0a9c6db1d940a1269123d183a5d36a6fcd
Ali Majed HA <ali.majedha.business@gmail.com>
2026-01-12 14:15:00 +0330

    initial project according to readme file

---
+++ NGErp.Warehouse.Service/Mappings/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace NGErp.Warehouse.Service.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+
+    }
+}
