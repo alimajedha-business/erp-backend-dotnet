3983689e7430535dbce2d89bdbbf42775f902947
Ali Majed HA <ali.majedha.business@gmail.com>
2026-01-20 14:56:27 +0330

    add dependency injection to service collection

--- Src/WarehouseModule/Warehouse.Service/ServiceCollectionExtensions.cs
+++ Src/WarehouseModule/Warehouse.Service/ServiceCollectionExtensions.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
-using NGErp.Base.Service.Interfaces;
-using NGErp.Base.Service.Services;
-using NGErp.General.Service.Services;
 using NGErp.Warehouse.Service.Mappings;
-using NGErp.Warehouse.Service.Resources;
-
+using NGErp.Warehouse.Service.Services;
 
 namespace NGErp.Warehouse.Service;
 
@@ -13,10 +9,8 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddWarehouseServices(this IServiceCollection services)
     {
-
         services.AddAutoMapper(typeof(MappingProfile));
-        services.AddScoped<IExceptionLocalizer<WarehouseResources>, ExceptionLocalizer<WarehouseResources>>();
-        services.AddScoped<IPythonIntegrationService, PythonIntegrationService>();
+        services.AddScoped<ICategoryService,  CategoryService>();
         return services;
     }
 }
