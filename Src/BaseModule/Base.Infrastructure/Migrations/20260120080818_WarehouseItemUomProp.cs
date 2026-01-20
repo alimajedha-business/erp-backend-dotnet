c6553f7b97b728ba299bba44c9f10925ad86db9e
Ali Majed HA <ali.majedha.business@gmail.com>
2026-01-20 11:38:36 +0330

    add migrations

---
+++ Src/BaseModule/Base.Infrastructure/Migrations/20260120080818_WarehouseItemUomProp.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NGErp.Base.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class WarehouseItemUomProp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDefalulIssue",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurement",
+                newName: "IsDefaultlIssue");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDefaultlIssue",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurement",
+                newName: "IsDefalulIssue");
+        }
+    }
+}
