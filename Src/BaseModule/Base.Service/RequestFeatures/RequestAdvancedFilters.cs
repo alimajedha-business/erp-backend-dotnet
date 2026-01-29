9776f10c7f806e0b40dcb9707dc1d2c31386db76
Ali Majed HA <ali.majedha.business@gmail.com>
2026-01-29 10:37:53 +0330

    add advanced-filtering class

---
+++ Src/BaseModule/Base.Service/RequestFeatures/RequestAdvancedFilters.cs
@@ -0,0 +1,10 @@
+﻿namespace NGErp.Base.Service.RequestFeatures;
+
+public record RequestAdvancedFilters
+{
+    public string? Predicate { get; set; }
+    public object[]? Args { get; set; }
+
+    public void Deconstruct(out string? predicate, out object[]? args)
+        => (predicate, args) = (Predicate, Args);
+}
