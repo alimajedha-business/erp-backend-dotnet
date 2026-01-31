543302e84aea1040b6ea9fa201f818e4234e618b
Ali Majed HA <ali.majedha.business@gmail.com>
2026-01-31 12:01:06 +0330

    remove unused using directives

--- Src/BaseModule/Base.Infrastructure/DataAccess/TypeExtensionMethod.cs
+++ Src/BaseModule/Base.Infrastructure/DataAccess/TypeExtensionMethod.cs
@@ -1,39 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Text;
+﻿namespace NGErp.Base.Infrastructure.DataAccess;
 
-namespace NGErp.Base.Infrastructure.DataAccess
+public static class TypeExtensionMethod
 {
-    public static class TypeExtensionMethod
+    public static bool IsAssignableToGenericType(this Type givenType, Type genericType)
     {
-        public static bool IsAssignableToGenericType(this Type givenType, Type genericType)
+        var interfaceTypes = givenType.GetInterfaces();
+
+        foreach (var it in interfaceTypes)
         {
-            var interfaceTypes = givenType.GetInterfaces();
-
-            foreach (var it in interfaceTypes)
-            {
-                if (it.IsGenericType && it.GetGenericTypeDefinition() == genericType)
-                    return true;
-            }
-
-            if (givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+            if (it.IsGenericType && it.GetGenericTypeDefinition() == genericType)
                 return true;
-
-            var baseType = givenType.BaseType;
-            if (baseType == null)
-                return false;
-
-            return IsAssignableToGenericType(baseType, genericType);
         }
-        public static object GetEnumValue(Type propertyType)
-        {
-            Random rnd = new Random();
-            var lst = Enum.GetValues(propertyType);
-            int arrnum = rnd.Next(lst.Length);
-            return lst.GetValue(arrnum)!;
-        }
-      
+
+        if (givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+            return true;
+
+        var baseType = givenType.BaseType;
+        if (baseType == null)
+            return false;
+
+        return IsAssignableToGenericType(baseType, genericType);
     }
+    public static object GetEnumValue(Type propertyType)
+    {
+        Random rnd = new Random();
+        var lst = Enum.GetValues(propertyType);
+        int arrnum = rnd.Next(lst.Length);
+        return lst.GetValue(arrnum)!;
+    }
+  
 }
 
