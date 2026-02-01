bd6744482f5306eb180b3820e8e7aa2cb21d7463
Ali Majed HA <ali.majedha.business@gmail.com>
2026-02-01 10:51:31 +0330

    add company-aware repository

---
+++ Src/GeneralModule/General.Infrastructure/DataAccess/Repositories/IRepositoryWithCompany.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+using NGErp.Base.Infrastructure.DataAccess.Repositories;
+using NGErp.Base.Service.RequestFeatures;
+using NGErp.General.Domain.Entities;
+
+namespace NGErp.General.Infrastructure.DataAccess.Repositories;
+
+public interface IRepositoryWithCompany<T> 
+    : IRepository<T> where T : BaseEntityWithCompany
+{
+    Task<T?> GetByIdAsync(Guid companyId, Guid id);
+    IQueryable<T> GetList(
+        Guid companyId,
+        RequestAdvancedFilters? requestAdvancedFilters = null,
+        IQueryable<T>? baseQuery = null
+    );
+    IQueryable<T> Find(
+        Guid companyId,
+        Expression<Func<T, bool>> predicate,
+        IQueryable<T>? baseQuery = null
+    );
+}
---
+++ Src/GeneralModule/General.Infrastructure/DataAccess/Repositories/RepositoryWithCompany.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
+
+using Microsoft.EntityFrameworkCore;
+
+using NGErp.Base.Infrastructure.DataAccess;
+using NGErp.Base.Infrastructure.DataAccess.Repositories;
+using NGErp.Base.Service.RequestFeatures;
+using NGErp.General.Domain.Entities;
+
+namespace NGErp.General.Infrastructure.DataAccess.Repositories;
+
+public class RepositoryWithCompany<T>(MainDbContext context) : 
+    Repository<T>(context),
+    IRepositoryWithCompany<T> where T : BaseEntityWithCompany
+{
+    public virtual async Task<T?> GetByIdAsync(Guid companyId, Guid id)
+    {
+        return await _dbSet
+            .Where(e => e.CompanyId == companyId && e.Id == id)
+            .SingleOrDefaultAsync();
+    }
+
+    public virtual IQueryable<T> GetList(
+        Guid companyId,
+        RequestAdvancedFilters? requestAdvancedFilters = null,
+        IQueryable<T>? baseQuery = null
+    )
+    {
+        IQueryable<T> query = baseQuery ?? _context
+            .Set<T>()
+            .Where(e => e.CompanyId == companyId);
+
+        if (requestAdvancedFilters != null)
+        {
+            var (search, args) = requestAdvancedFilters;
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                query = (args is { Length: > 0 })
+                    ? query.Where(search, args)
+                    : query.Where(search);
+            }
+        }
+
+        return query;
+    }
+
+    public virtual IQueryable<T> Find(
+        Guid companyId,
+        Expression<Func<T, bool>> predicate,
+        IQueryable<T>? baseQuery = null
+    )
+    {
+        return (baseQuery ?? _dbSet)
+            .Where(e => e.CompanyId == companyId)
+            .Where(predicate);
+    }
+}
