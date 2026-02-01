dfebcaf8d89a0e4fa7c8398690f3b38d2a7d5d48
Ali Majed HA <ali.majedha.business@gmail.com>
2026-02-01 09:19:09 +0330

    modify access modifier for setter method in base-entity-with-company model

--- Src/GeneralModule/General.Domain/Entities/BaseEntityWithCompany.cs
+++ Src/GeneralModule/General.Domain/Entities/BaseEntityWithCompany.cs
@@ -1,15 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using NGErp.Base.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NGErp.General.Domain.Entities;
 
 public abstract class BaseEntityWithCompany : BaseEntity
 {
-    public Guid CompanyId { get; private set; }
+    public Guid CompanyId { get; set; }
 
-    
     public static new void MapBase(EntityTypeBuilder builder)
     {
         builder
