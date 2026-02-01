594f916697cc0a93f080192a536ffaca2b434d59
Ali Majed HA <ali.majedha.business@gmail.com>
2026-02-01 09:18:19 +0330

    use primary constructor

--- Src/BaseModule/Base.Infrastructure/DataAccess/Repositories/IRepository.cs
+++ Src/BaseModule/Base.Infrastructure/DataAccess/Repositories/IRepository.cs
@@ -2,27 +2,26 @@ using System.Linq.Expressions;
 
 using NGErp.Base.Service.RequestFeatures;
 
-namespace NGErp.Base.Infrastructure.DataAccess.Repositories
+namespace NGErp.Base.Infrastructure.DataAccess.Repositories;
+
+public interface IRepository<T> where T : class
 {
-    public interface IRepository<T> where T : class
-    {
-        Task<T?> GetByIdAsync(Guid id);
-        IQueryable<T> GetList(
-            RequestAdvancedFilters? requestAdvancedFilters = null,
-            IQueryable<T>? baseQuery = null
-        );
-        IQueryable<T> Find(
-            Expression<Func<T, bool>> predicate,
-            IQueryable<T>? baseQuery = null
-        );
-        Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
-        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
-        Task<T> AddAsync(T entity, CancellationToken ct);
-        Task AddRangeAsync(IEnumerable<T> entities);
-        void Update(T entity);
-        void UpdateRange(IEnumerable<T> entities);
-        void Remove(T entity);
-        void RemoveRange(IEnumerable<T> entities);
-        Task<int> SaveChangesAsync(CancellationToken ct);
-    }
+    Task<T?> GetByIdAsync(Guid id);
+    IQueryable<T> GetList(
+        RequestAdvancedFilters? requestAdvancedFilters = null,
+        IQueryable<T>? baseQuery = null
+    );
+    IQueryable<T> Find(
+        Expression<Func<T, bool>> predicate,
+        IQueryable<T>? baseQuery = null
+    );
+    Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+    Task<T> AddAsync(T entity, CancellationToken ct);
+    Task AddRangeAsync(IEnumerable<T> entities);
+    void Update(T entity);
+    void UpdateRange(IEnumerable<T> entities);
+    void Remove(T entity);
+    void RemoveRange(IEnumerable<T> entities);
+    Task<int> SaveChangesAsync(CancellationToken ct);
 }
