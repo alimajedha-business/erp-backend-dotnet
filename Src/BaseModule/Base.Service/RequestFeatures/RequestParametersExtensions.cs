bcaf6f218d57fbcaac04eec7d81e2289a583b608
Ali Majed HA <ali.majedha.business@gmail.com>
2026-02-03 12:23:22 +0330

    add extensions to concat search params with query params into a single predicate-args

---
+++ Src/BaseModule/Base.Service/RequestFeatures/RequestParametersExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+using Microsoft.AspNetCore.Http;
+
+namespace NGErp.Base.Service.RequestFeatures;
+
+public static class RequestParametersExtensions
+{
+    public static Dictionary<string, string> GetEntitySpecificQueryParams<TParams>(
+        this IQueryCollection query
+    )
+        where TParams : RequestParameters
+    {
+        var baseProps = typeof(RequestParameters)
+            .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            .Select(p => p.Name)
+            .ToHashSet(StringComparer.OrdinalIgnoreCase);
+
+        return query
+            .Where(kvp => !baseProps.Contains(kvp.Key))
+            .ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToString(), StringComparer.OrdinalIgnoreCase);
+    }
+}
