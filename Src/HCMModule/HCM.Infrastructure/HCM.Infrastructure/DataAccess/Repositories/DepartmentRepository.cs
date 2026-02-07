1d8b250001889ed63e835130bb1c1b679e5df919
Ali Majed HA <ali.majedha.business@gmail.com>
2026-02-07 14:11:27 +0330

    fix get-all method for department

--- Src/HCMModule/HCM.Infrastructure/HCM.Infrastructure/DataAccess/Repositories/DepartmentRepository.cs
+++ Src/HCMModule/HCM.Infrastructure/HCM.Infrastructure/DataAccess/Repositories/DepartmentRepository.cs
@@ -1,34 +1,11 @@
-﻿
-using Microsoft.EntityFrameworkCore;
-
-using NGErp.Base.Infrastructure.DataAccess;
-using NGErp.Base.Service.RequestFeatures;
-using NGErp.Base.Service.ResponseModels;
+﻿using NGErp.Base.Infrastructure.DataAccess;
 using NGErp.General.Infrastructure.DataAccess.Repositories;
 using NGErp.HCM.Domain.Entities;
 using NGErp.HCM.Service.Repository.Contracts;
-using NGErp.HCM.Service.RequestFeatures;
 
 namespace NGErp.HCM.Infrastructure.DataAccess.Repositories;
 
 public class DepartmentRepository(MainDbContext context) :
     RepositoryWithCompany<Department>(context),
     IDepartmentRepository
-{
-    public async Task<ListQueryResult<Department>> GetAllAsync(
-        Guid companyId,
-        DepartmentParameters departmentParameters,
-        CancellationToken ct,
-        RequestAdvancedFilters? requestAdvancedFilters = null
-        )
-    {
-        IQueryable<Department> sorted = base
-            .GetAll(companyId, requestAdvancedFilters)
-            .Sort(departmentParameters);
-
-        var totalCount = await sorted.CountAsync(ct);
-        var items = await sorted.Paginate(departmentParameters).ToListAsync(ct);
-
-        return new ListQueryResult<Department>(items, totalCount);
-    }
-}
+{ }
--- Src/HCMModule/HCM.Service/HCM.Service/Repository.Contracts/IDepartmentRepository.cs
+++ Src/HCMModule/HCM.Service/HCM.Service/Repository.Contracts/IDepartmentRepository.cs
@@ -1,26 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using NGErp.Base.Service.Repository.Contract;
-using NGErp.Base.Service.RequestFeatures;
-using NGErp.Base.Service.ResponseModels;
-using NGErp.General.Service.Repository.Contracts;
+﻿using NGErp.General.Service.Repository.Contracts;
 using NGErp.HCM.Domain.Entities;
-using NGErp.HCM.Service.RequestFeatures;
-
 
 namespace NGErp.HCM.Service.Repository.Contracts;
 
-public interface IDepartmentRepository : IRepositoryWithCompany<Department>
-{    
-    Task<ListQueryResult<Department>> GetAllAsync(
-        Guid companyId,
-        DepartmentParameters departmentParameters,
-        CancellationToken ct,
-        RequestAdvancedFilters? requestAdvancedFilters = null
-    );
-
-}
+public interface IDepartmentRepository : IRepositoryWithCompany<Department> { }
