d6b38ba9bd52705b6ec4b09867e6e92506b4c8c9
Ali Majed HA <ali.majedha.business@gmail.com>
2026-02-07 16:50:12 +0330

    add exception class if the user is not present in request

---
+++ Src/BaseModule/Base.Domain/Exceptions/InvalidUserBadRequestException.cs
@@ -0,0 +1,8 @@
+﻿namespace NGErp.Base.Domain.Exceptions;
+
+public class InvalidUserBadRequestException : BadRequestException
+{
+    public InvalidUserBadRequestException() : base("Invalid user")
+    {
+    }
+}
