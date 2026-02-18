dad2e55a0cc453cd967a01e4333cd20c016abcd7
Ali Majed HA <ali.majedha.business@gmail.com>
2026-02-18 09:33:39 +0330

    refactor item apis and services according to base service changes

--- Src/WarehouseModule/Warehouse.Service/Services/IItemService.cs
+++ Src/WarehouseModule/Warehouse.Service/Services/IItemService.cs
@@ -1,46 +1,27 @@
-﻿using Microsoft.AspNetCore.JsonPatch;
-
-using NGErp.Base.Service.DTOs;
+﻿using NGErp.Base.Service.DTOs;
 using NGErp.Base.Service.ResponseModels;
+using NGErp.General.Service.Services;
+using NGErp.Warehouse.Domain.Entities;
 using NGErp.Warehouse.Service.DTOs;
+using NGErp.Warehouse.Service.Repository.Contracts;
 using NGErp.Warehouse.Service.RequestFeatures;
+using NGErp.Warehouse.Service.Resources;
 
 namespace NGErp.Warehouse.Service.Services;
 
-public interface IItemService
+public interface IItemService : IBaseServiceWithCompany<
+    Item,
+    ItemDto,
+    ItemParameters,
+    IItemRepository,
+    WarehouseResource
+>
 {
-    Task<ItemDto> CreateItemAsync(
-        Guid companyId,
-        CreateItemDto createDto,
-        CancellationToken ct
-    );
-    Task<ListResponseModel<ItemDto>> GetAllItemsAsync(
-        Guid companyId,
-        ItemParameters parameters,
-        CancellationToken ct,
-        FilterNodeDto? filterNodeDto = null
-    );
     Task<ListResponseModel<ItemDto>> GetCategoryAllItemsAsync(
         Guid companyId,
         Guid categoryId,
         ItemParameters parameters,
         CancellationToken ct,
         FilterNodeDto? filterNodeDto = null
-    );
-    Task<ItemDto> GetItemByIdAsync(
-        Guid companyId,
-        Guid id,
-        CancellationToken ct
-    );
-    Task<ItemDto> PatchItemAsync(
-        Guid companyId,
-        Guid id,
-        JsonPatchDocument<PatchItemDto> patchDocument,
-        CancellationToken ct
-    );
-    Task DeleteItemAsync(
-        Guid companyId,
-        Guid id,
-        CancellationToken ct
     );
 }
