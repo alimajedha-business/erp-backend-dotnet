6a10423972ce82694e9ecb65c96ce995d04ec37f
Ali Majed HA <ali.majedha.business@gmail.com>
2026-02-28 09:38:03 +0330

    modify company unit entity not to inherit from base enity

--- Src/GeneralModule/General.Domain/Entities/CompanyUnit.cs
+++ Src/GeneralModule/General.Domain/Entities/CompanyUnit.cs
@@ -1,33 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-using NGErp.Base.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NGErp.General.Domain.Entities;
 
-public class CompanyUnit :
-    BaseEntityWithCompany,
-    IBaseEntityTypeConfiguration<CompanyUnit>
+[Table("company_units", Schema = "Shared")]
+public class CompanyUnit
 {
+    public Guid Id { get; set; }
     public string Code { get; private set; } = default!;
     public string Name { get; private set; } = default!;
-    public bool IsActive { get; private set; } = true;
-
-    public void Map(EntityTypeBuilder<CompanyUnit> builder)
-    {
-        builder
-            .ToTable(nameof(CompanyUnit), "General");
-
-        builder
-            .Property(e => e.Code)
-            .HasMaxLength(50);
-
-        builder
-            .Property(e => e.Name)
-            .HasMaxLength(250);
-
-        builder
-            .Property(e => e.IsActive)
-            .HasDefaultValue(true);
-    }
 }
