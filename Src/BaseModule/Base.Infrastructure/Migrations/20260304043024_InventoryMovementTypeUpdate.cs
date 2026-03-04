041b6f0aaf5a9493c2c0d1c580a230f6f3bf4598
Ali Majed HA <ali.majedha.business@gmail.com>
2026-03-04 08:01:30 +0330

    add migrations

---
+++ Src/BaseModule/Base.Infrastructure/Migrations/20260304043024_InventoryMovementTypeUpdate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NGErp.Base.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InventoryMovementTypeUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IncreaseStockQuantity",
+                schema: "Warehouse",
+                table: "InventoryMovementType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IncreaseStockQuantity",
+                schema: "Warehouse",
+                table: "InventoryMovementType",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+        }
+    }
+}
