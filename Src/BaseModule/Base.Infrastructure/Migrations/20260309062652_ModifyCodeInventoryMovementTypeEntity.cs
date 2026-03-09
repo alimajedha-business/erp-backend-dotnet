4f1316bfbe1183604ebd54694ddcbdedb32cb18f
Ali Majed HA <ali.majedha.business@gmail.com>
2026-03-09 09:57:48 +0330

    add migration

---
+++ Src/BaseModule/Base.Infrastructure/Migrations/20260309062652_ModifyCodeInventoryMovementTypeEntity.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NGErp.Base.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyCodeInventoryMovementTypeEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Code",
+                schema: "Warehouse",
+                table: "InventoryMovementType",
+                type: "int",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                schema: "Warehouse",
+                table: "InventoryMovementType",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 20);
+        }
+    }
+}
