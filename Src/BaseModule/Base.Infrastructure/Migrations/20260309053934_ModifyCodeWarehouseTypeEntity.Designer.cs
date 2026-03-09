f4f0281c94a1042f411bcb9a10b0af18a887430f
Ali Majed HA <ali.majedha.business@gmail.com>
2026-03-09 09:11:03 +0330

    add migration

--- Src/BaseModule/Base.Infrastructure/Migrations/20260309053934_ModifyCodeWarehouseTypeEntity.Designer.cs
+++ Src/BaseModule/Base.Infrastructure/Migrations/20260309053934_ModifyCodeWarehouseTypeEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NGErp.Base.Infrastructure.DataAccess;
 
@@ -11,9 +12,11 @@ using NGErp.Base.Infrastructure.DataAccess;
 namespace NGErp.Base.Infrastructure.Migrations
 {
     [DbContext(typeof(MainDbContext))]
-    partial class MainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260309053934_ModifyCodeWarehouseTypeEntity")]
+    partial class ModifyCodeWarehouseTypeEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
