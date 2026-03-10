5dc0ec041d777502a8722f48def899cda3c136aa
Ali Majed HA <ali.majedha.business@gmail.com>
2026-03-10 08:57:50 +0330

    add migration

---
+++ Src/BaseModule/Base.Infrastructure/Migrations/20260310052651_ModifyNullabilityForSecondaryEntities.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NGErp.Base.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyNullabilityForSecondaryEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDefaultPurchase",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurement",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDefaultIssue",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurement",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsBase",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurement",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDefaultPurchase",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurement",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDefaultIssue",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurement",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsBase",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurement",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+        }
+    }
+}
