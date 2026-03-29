4db857b95329ca88f8597c5510a503de30857aa5
Ali Majed HA <ali.majedha.business@gmail.com>
2026-03-29 14:14:48 +0330

    add shipping company dtos and mappings

---
+++ Src/WarehouseModule/Warehouse.Service/Mappings/ShippingCompanyMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+
+using NGErp.Warehouse.Domain.Entities;
+using NGErp.Warehouse.Service.DTOs;
+
+namespace NGErp.Warehouse.Service.Mappings;
+
+public class ShippingCompanyMappingProfile : Profile
+{
+    public ShippingCompanyMappingProfile()
+    {
+        CreateMap<ShippingCompany, ShippingCompanyDto>();
+        CreateMap<ShippingCompany, ShippingCompanyListDto>()
+            .ForCtorParam(
+                nameof(ShippingCompanyListDto.ManagerFullName),
+                opt => opt.MapFrom(src => src.ManagerFirstName + ' ' + src.ManagerLastName)
+            );
+        CreateMap<CreateShippingCompanyDto, ShippingCompany>();
+        CreateMap<PatchShippingCompanyDto, ShippingCompany>().ReverseMap();
+    }
+}
