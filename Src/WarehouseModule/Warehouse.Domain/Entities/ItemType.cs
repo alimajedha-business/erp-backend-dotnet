5ff7d6300a1fe471a95ed71c9890afdc39b0aa05
Ali Majed HA <ali.majedha.business@gmail.com>
2026-03-30 08:27:38 +0330

    add item type entity

---
+++ Src/WarehouseModule/Warehouse.Domain/Entities/ItemType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using NGErp.Base.Domain.Entities;
+
+namespace NGErp.Warehouse.Domain.Entities;
+
+public class ItemType :
+    BaseEntity,
+    IBaseEntityTypeConfiguration<ItemType>
+{
+    public required int Code { get; set; }
+    public required string Title { get; set; }
+
+    public void Map(EntityTypeBuilder<ItemType> builder)
+    {
+        builder.
+            ToTable(nameof(ItemType), "Warehouse");
+
+        builder
+            .HasIndex(i => new { i.Code })
+            .IsUnique()
+            .HasDatabaseName("UX_ItemType_Code");
+
+        builder
+            .Property(e => e.Title)
+            .HasMaxLength(200);
+    }
+}
