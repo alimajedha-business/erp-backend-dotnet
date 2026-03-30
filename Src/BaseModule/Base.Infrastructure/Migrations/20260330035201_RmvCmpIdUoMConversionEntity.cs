5a12750de8ad2e0e1b0eb549394453f040848ebb
Ali Majed HA <ali.majedha.business@gmail.com>
2026-03-30 08:27:13 +0330

    add migrations

---
+++ Src/BaseModule/Base.Infrastructure/Migrations/20260330035201_RmvCmpIdUoMConversionEntity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NGErp.Base.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RmvCmpIdUoMConversionEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UnitOfMeasurementConversion_Companies_CompanyId",
+                schema: "Warehouse",
+                table: "UnitOfMeasurementConversion");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UnitOfMeasurementConversion_CompanyId",
+                schema: "Warehouse",
+                table: "UnitOfMeasurementConversion");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                schema: "Warehouse",
+                table: "UnitOfMeasurementConversion");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CompanyId",
+                schema: "Warehouse",
+                table: "UnitOfMeasurementConversion",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UnitOfMeasurementConversion_CompanyId",
+                schema: "Warehouse",
+                table: "UnitOfMeasurementConversion",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UnitOfMeasurementConversion_Companies_CompanyId",
+                schema: "Warehouse",
+                table: "UnitOfMeasurementConversion",
+                column: "CompanyId",
+                principalSchema: "General",
+                principalTable: "Companies",
+                principalColumn: "Id");
+        }
+    }
+}
