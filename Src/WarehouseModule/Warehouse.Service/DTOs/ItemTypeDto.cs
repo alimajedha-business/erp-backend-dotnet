6685e6cb340b9174cc09f00c0044a0b0f1fdaaec
Ali Majed HA <ali.majedha.business@gmail.com>
2026-03-30 08:29:24 +0330

    add item type dtos and mappings

---
+++ Src/WarehouseModule/Warehouse.Service/DTOs/ItemTypeDto.cs
@@ -0,0 +1,19 @@
+﻿namespace NGErp.Warehouse.Service.DTOs;
+
+public record ItemTypeDto(
+    Guid Id,
+    int Code,
+    string Title
+);
+
+public class CreateItemTypeDto
+{
+    public required int Code { get; set; }
+    public required string Title { get; set; }
+}
+
+public class PatchItemTypeDto
+{
+    public int? Code { get; set; }
+    public string? Title { get; set; }
+}
