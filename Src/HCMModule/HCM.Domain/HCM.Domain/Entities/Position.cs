ba6650c056ef49edc563c0935c27228df00166e8
Rafiei-Tolid <rafiei.alireza@yahoo.com>
2026-03-31 10:21:13 +0330

    add constraints to Department, Position

--- Src/HCMModule/HCM.Domain/HCM.Domain/Entities/Position.cs
+++ Src/HCMModule/HCM.Domain/HCM.Domain/Entities/Position.cs
@@ -29,10 +29,18 @@ namespace NGErp.HCM.Domain.Entities
 
         public void Map(EntityTypeBuilder<Position> builder)
         {
-            builder.ToTable("Position", "HCM");
+            builder.ToTable("Position", "HCM")
+                .ToTable(t => t.HasCheckConstraint(
+                "CK_Position_StatusChangeDate",
+                "([Status] = 0 AND [StatusChangeDate] IS NOT NULL) OR ([Status] = 1 AND [StatusChangeDate] IS NULL)"
+            ));
             builder.Property(e => e.Name).HasMaxLength(200);
             builder.Property(e => e.Description).HasMaxLength(500);
             builder.Property(e => e.Code).HasMaxLength(100);
+            builder
+                .HasIndex(i => new { i.CompanyId, i.Code })
+                .IsUnique()
+                .HasDatabaseName("UX_Position_CompanyId_Code");
         }
     }
 }
