92d51320261eb37e30dbadab9b0f50b79c37e795
Ali Majed HA <ali.majedha.business@gmail.com>
2026-04-05 07:58:54 +0330

    add validation classes

---
+++ Src/WarehouseModule/Warehouse.Service/RequestValidators/ShippingCompanyValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+using Microsoft.Extensions.Localization;
+
+using NGErp.Warehouse.Domain.Entities;
+using NGErp.Warehouse.Service.Resources;
+
+namespace NGErp.Warehouse.Service.RequestValidators;
+
+public class ShippingCompanyValidator : AbstractValidator<ShippingCompany>
+{
+    private readonly IStringLocalizer<WarehouseResource> _localizer;
+
+    public ShippingCompanyValidator(IStringLocalizer<WarehouseResource> localizer)
+    {
+        _localizer = localizer;
+    }
+}
---
+++ Src/WarehouseModule/Warehouse.Service/RequestValidators/WarehouseLocationValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+using Microsoft.Extensions.Localization;
+
+using NGErp.Warehouse.Domain.Entities;
+using NGErp.Warehouse.Service.Resources;
+
+namespace NGErp.Warehouse.Service.RequestValidators;
+
+public class WarehouseLocationValidator(
+    IStringLocalizer<WarehouseResource> localizer
+) : AbstractValidator<WarehouseLocation>
+{
+    private readonly IStringLocalizer<WarehouseResource> _localizer = localizer;
+}
