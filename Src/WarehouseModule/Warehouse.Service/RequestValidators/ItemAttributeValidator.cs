34bf3e1783006c846a12d04f0a42a5517b851879
Ali Majed HA <ali.majedha.business@gmail.com>
2026-04-07 07:32:24 +0330

    add/modify validators, parameters, and request examples

---
+++ Src/WarehouseModule/Warehouse.Service/RequestValidators/ItemAttributeValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+using NGErp.Warehouse.Domain.Entities;
+
+namespace NGErp.Warehouse.Service.RequestValidators;
+
+public class ItemAttributeValidator : AbstractValidator<ItemAttribute>
+{
+    public ItemAttributeValidator()
+    {
+
+    }
+}
---
+++ Src/WarehouseModule/Warehouse.Service/RequestValidators/ItemUnitOfMeasurementValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+using NGErp.Warehouse.Domain.Entities;
+
+namespace NGErp.Warehouse.Service.RequestValidators;
+
+public class ItemUnitOfMeasurementValidator :
+    AbstractValidator<ItemUnitOfMeasurement>
+{
+    public ItemUnitOfMeasurementValidator()
+    {
+
+    }
+}
