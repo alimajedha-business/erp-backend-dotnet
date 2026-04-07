1cfea49271618ab71fce3ac43da0684a1e61deb3
Ali Majed HA <ali.majedha.business@gmail.com>
2026-04-07 07:24:55 +0330

    add/modify dtos and mapping profiles related to item entity, modify item dto

---
+++ Src/WarehouseModule/Warehouse.Service/DTOs/ItemAttributeDto.cs
@@ -0,0 +1,18 @@
+﻿namespace NGErp.Warehouse.Service.DTOs;
+
+public record ItemAttributeDto(
+    ItemDto Item,
+    AttributeDto Attribute
+);
+
+public class CreateItemAttributeDto
+{
+    public required Guid ItemId { get; set; }
+    public required Guid AttributeId { get; set; }
+}
+
+public class PatchItemAttributeDto
+{
+    public Guid? ItemId { get; set; }
+    public Guid? AttributeId { get; set; }
+}
---
+++ Src/WarehouseModule/Warehouse.Service/Mappings/ItemUnitOfMeasurementMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+using NGErp.Warehouse.Domain.Entities;
+using NGErp.Warehouse.Service.DTOs;
+
+namespace NGErp.Warehouse.Service.Mappings;
+
+public class ItemUnitOfMeasurementMappingProfile : Profile
+{
+    public ItemUnitOfMeasurementMappingProfile()
+    {
+        CreateMap<ItemUnitOfMeasurement, ItemUnitOfMeasurementDto>();
+        CreateMap<CreateItemUnitOfMeasurementDto, ItemUnitOfMeasurement>();
+        CreateMap<PatchItemUnitOfMeasurementDto, ItemUnitOfMeasurement>().ReverseMap();
+    }
+}
