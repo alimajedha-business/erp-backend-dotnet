f7b7fde8d8c8b0471fe2ba82b95cb2df92fa791b
Ali Majed HA <ali.majedha.business@gmail.com>
2026-04-07 07:25:29 +0330

    add/modify schemas related to item entity

---
+++ Src/WarehouseModule/Warehouse.Domain/EntitySchemas/ItemAttributeSchema.cs
@@ -0,0 +1,13 @@
+﻿using NGErp.Base.Domain.EntitySchemas;
+using NGErp.Warehouse.Domain.Entities;
+
+namespace NGErp.Warehouse.Domain.EntitySchemas;
+
+public sealed class ItemAttributeSchema : IFilterSchema<ItemAttribute>
+{
+    public FilterSchema Build()
+    {
+        var filterSchema = new FilterSchema();
+        return filterSchema;
+    }
+}
