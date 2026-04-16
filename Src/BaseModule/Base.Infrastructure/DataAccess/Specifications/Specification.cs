6adfcbb1589ec158e6a10d807f996dfe10a8b9a8
Ali Majed HA <ali.majedha.business@gmail.com>
2026-04-16 11:41:52 +0330

    remove specification due to being positioned in incorrect application

--- Src/BaseModule/Base.Infrastructure/DataAccess/Specifications/Specification.cs
+++
@@ -1,9 +0,0 @@
-﻿using NGErp.Base.Service.Repository.Contracts;
-
-namespace NGErp.Base.Infrastructure.DataAccess.Specifications;
-
-public class Specification<T>(Func<IQueryable<T>, IQueryable<T>> query)
-    : ISpecification<T>
-{
-    public Func<IQueryable<T>, IQueryable<T>> Query { get; } = query;
-}
