c6599abae9feba6f2d22eb2360e5ff17fccbc913
Ali Majed HA <ali.majedha.business@gmail.com>
2026-04-18 11:44:58 +0330

    improve exception handling middleware

--- Src/API/API/Services/ExceptionLocalizerFactory.cs
+++ Src/API/API/Services/ExceptionLocalizerFactory.cs
@@ -1,6 +1,7 @@
-﻿using NGErp.Base.Service.Resources;
+using NGErp.Base.Service.Resources;
 using NGErp.Base.Service.Services;
 using NGErp.General.Service.Resources;
+using NGErp.HCM.Service.Resources;
 using NGErp.Warehouse.Service.Resources;
 
 namespace NGErp.API.Services
@@ -14,10 +15,11 @@ namespace NGErp.API.Services
             var mapping = new (string key, Type resource)[]
             {
                 ("General", typeof(GeneralResource)),
+                ("HCM", typeof(HCMResource)),
                 ("Warehouse", typeof(WarehouseResource)),
             };
 
-            var (key, resource) = mapping.FirstOrDefault(m => ns.Contains(m.key));
+            var (_, resource) = mapping.FirstOrDefault(m => ns.Contains(m.key));
 
             var resourceType = resource ?? typeof(BaseResource);
             var serviceType = typeof(IExceptionLocalizer<>).MakeGenericType(resourceType);
