0661d632cf8ef2fc3c7328db5a10df0f59fdc4d4
Ali Majed HA <ali.majedha.business@gmail.com>
2026-04-22 15:23:23 +0330

    add migration

---
+++ Src/BaseModule/Base.Infrastructure/Migrations/20260422115241_ItemUomConversionEqPrp.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NGErp.Base.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ItemUomConversionEqPrp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasNextLevel",
+                schema: "Warehouse",
+                table: "WarehouseLocation",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConversionEquation",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurementConversion",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasNextLevel",
+                schema: "Warehouse",
+                table: "WarehouseLocation");
+
+            migrationBuilder.DropColumn(
+                name: "ConversionEquation",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurementConversion");
+        }
+    }
+}
