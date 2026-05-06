cbcd7a2e5ac549793a4017bc78a577766746e374
Ali Majed HA <ali.majedha.business@gmail.com>
2026-05-06 12:44:35 +0330

    add migration

---
+++ Src/BaseModule/Base.Infrastructure/Migrations/20260506091326_ReceiptTypeEntity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NGErp.Base.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReceiptTypeEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReceiptType",
+                schema: "Warehouse",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    Code = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    AddToStock = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    TimeZone = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    ModifierId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CompanyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReceiptType", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReceiptType_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalSchema: "General",
+                        principalTable: "Companies",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceiptType_IsDeleted",
+                schema: "Warehouse",
+                table: "ReceiptType",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "UX_ReceiptType_Company_Code",
+                schema: "Warehouse",
+                table: "ReceiptType",
+                columns: new[] { "CompanyId", "Code" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReceiptType",
+                schema: "Warehouse");
+        }
+    }
+}
