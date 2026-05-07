c9d8a031b0062b32aadeec9e8626c0aa672a212d
Ali Majed HA <ali.majedha.business@gmail.com>
2026-05-07 08:32:20 +0330

    add receipt tables to the context

--- Src/BaseModule/Base.Infrastructure/DataAccess/MainDbContext.cs
+++ Src/BaseModule/Base.Infrastructure/DataAccess/MainDbContext.cs
@@ -95,7 +95,9 @@ namespace NGErp.Base.Infrastructure.DataAccess
         public virtual DbSet<ItemUnitOfMeasurement> ItemUnitOfMeasurements { get; set; }
         public virtual DbSet<ItemUnitOfMeasurementConversion> ItemUnitOfMeasurementConversions { get; set; }
         public virtual DbSet<RemittanceType> RemittanceTypes { get; set; }
+        public virtual DbSet<ReceiptFieldDefinition> ReceiptFieldDefinitions { get; set; }
         public virtual DbSet<ReceiptType> ReceiptTypes { get; set; }
+        public virtual DbSet<ReceiptTypeFieldConfiguration> ReceiptTypeFieldConfigurations { get; set; }
         public virtual DbSet<UnitOfMeasurement> UnitOfMeasurements { get; set; }
         public virtual DbSet<Warehouse.Domain.Entities.Warehouse> Warehouses { get; set; }
         public virtual DbSet<WarehouseLocation> WarehouseLocations { get; set; }
