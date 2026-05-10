93d00aa8405d0580fe079d18331da6a10c35cdb2
hosein <hsgry800@gmail.com>
2026-05-10 14:08:07 +0330

    add child to relative types.

---
+++ Src/BaseModule/Base.Infrastructure/Migrations/20260509091536_AddChildTypeToRelativeType.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NGErp.Base.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChildTypeToRelativeType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_RelativeType_Type",
+                schema: "HCM",
+                table: "RelativeType");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_RelativeType_Type",
+                schema: "HCM",
+                table: "RelativeType",
+                sql: "[Type] BETWEEN 1 AND 7");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_RelativeType_Type",
+                schema: "HCM",
+                table: "RelativeType");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_RelativeType_Type",
+                schema: "HCM",
+                table: "RelativeType",
+                sql: "[Type] BETWEEN 1 AND 6");
+        }
+    }
+}
