fe8de0cba022e1084cbd8011287862844191a5d0
Ali Majed HA <ali.majedha.business@gmail.com>
2026-05-12 08:44:35 +0330

    add migration

---
+++ Src/BaseModule/Base.Infrastructure/Migrations/20260512051414_ItemPropType.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NGErp.Base.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ItemPropType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Weigh",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurement",
+                newName: "Weight");
+
+            migrationBuilder.RenameColumn(
+                name: "CubeVolume",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurement",
+                newName: "Volume");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Weight",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurement",
+                newName: "Weigh");
+
+            migrationBuilder.RenameColumn(
+                name: "Volume",
+                schema: "Warehouse",
+                table: "ItemUnitOfMeasurement",
+                newName: "CubeVolume");
+        }
+    }
+}
