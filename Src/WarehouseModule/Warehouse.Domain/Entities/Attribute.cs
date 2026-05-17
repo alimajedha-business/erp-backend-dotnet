32faa171ea4ee27880300200a576d6625f542e6d
Ali Majed HA <ali.majedha.business@gmail.com>
2026-05-17 13:55:48 +0330

    add static-key to attribute

--- Src/WarehouseModule/Warehouse.Domain/Entities/Attribute.cs
+++ Src/WarehouseModule/Warehouse.Domain/Entities/Attribute.cs
@@ -22,6 +22,8 @@ public class Attribute :
     public bool IsRequired { get; set; } = false;
     public bool IsStockDimension {  get; set; } = false;
     public bool IsStatic { get; set; } = false;
+    public string StaticKey { get; set; } = default!;
+
 
     public ICollection<ItemAttribute> ItemAttributes { get; set; } = [];
 
