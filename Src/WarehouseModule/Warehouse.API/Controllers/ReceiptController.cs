df705e76d429d778da73093e4405c1ce459131db
Ali Majed HA <ali.majedha.business@gmail.com>
2026-05-17 14:54:16 +0330

    fix api type attribute

--- Src/WarehouseModule/Warehouse.API/Controllers/ReceiptController.cs
+++ Src/WarehouseModule/Warehouse.API/Controllers/ReceiptController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
-using NGErp.Base.API.ActionFilters;
+using NGErp.Base.Service.Authorization;
 using NGErp.Base.Service.DTOs;
 using NGErp.Base.Service.ResponseModels;
 using NGErp.Warehouse.Service.DTOs;
@@ -50,7 +50,7 @@ public class ReceiptController(
     }
 
     [HttpPost("list")]
-    [SkipModelValidation]
+    [InherentlyAction(ActionType.Read)]
     [SwaggerRequestExample(typeof(object), typeof(ReceiptAdvancedSearchExample))]
     [Produces("application/json")]
     [ProducesResponseType(typeof(ListResponseModel<ReceiptListDto>), StatusCodes.Status200OK)]
