8e2c7959880c5764f8876627bbca01840828aefc
Ali Majed HA <ali.majedha.business@gmail.com>
2026-05-17 16:50:12 +0330

    add migration

---
+++ Src/BaseModule/Base.Infrastructure/Migrations/20260517131927_FixInventoryLotAttributeValueRelation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NGErp.Base.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixInventoryLotAttributeValueRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InventoryLotAttributeValue_InventoryLot_InventoryLotId",
+                schema: "Warehouse",
+                table: "InventoryLotAttributeValue");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InventoryLotAttributeValue_InventoryLotId",
+                schema: "Warehouse",
+                table: "InventoryLotAttributeValue");
+
+            migrationBuilder.DropColumn(
+                name: "InventoryLotId",
+                schema: "Warehouse",
+                table: "InventoryLotAttributeValue");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "InventoryLotId",
+                schema: "Warehouse",
+                table: "InventoryLotAttributeValue",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryLotAttributeValue_InventoryLotId",
+                schema: "Warehouse",
+                table: "InventoryLotAttributeValue",
+                column: "InventoryLotId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InventoryLotAttributeValue_InventoryLot_InventoryLotId",
+                schema: "Warehouse",
+                table: "InventoryLotAttributeValue",
+                column: "InventoryLotId",
+                principalSchema: "Warehouse",
+                principalTable: "InventoryLot",
+                principalColumn: "Id");
+        }
+    }
+}
