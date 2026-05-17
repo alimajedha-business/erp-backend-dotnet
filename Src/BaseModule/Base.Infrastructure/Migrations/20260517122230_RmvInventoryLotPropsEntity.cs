dc1a0bedf4ab67b4e02f13fe295e3db5c734a8f6
Ali Majed HA <ali.majedha.business@gmail.com>
2026-05-17 15:53:01 +0330

    add migration

---
+++ Src/BaseModule/Base.Infrastructure/Migrations/20260517122230_RmvInventoryLotPropsEntity.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NGErp.Base.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RmvInventoryLotPropsEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UX_InvLot_Item_DimHash",
+                schema: "Warehouse",
+                table: "InventoryLot");
+
+            migrationBuilder.DropColumn(
+                name: "LotNumber",
+                schema: "Warehouse",
+                table: "InventoryLot");
+
+            migrationBuilder.DropColumn(
+                name: "SerialNumber",
+                schema: "Warehouse",
+                table: "InventoryLot");
+
+            migrationBuilder.CreateIndex(
+                name: "UX_InvLot_Item_DimHash",
+                schema: "Warehouse",
+                table: "InventoryLot",
+                columns: new[] { "ItemId", "StockKeyHash" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UX_InvLot_Item_DimHash",
+                schema: "Warehouse",
+                table: "InventoryLot");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LotNumber",
+                schema: "Warehouse",
+                table: "InventoryLot",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SerialNumber",
+                schema: "Warehouse",
+                table: "InventoryLot",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UX_InvLot_Item_DimHash",
+                schema: "Warehouse",
+                table: "InventoryLot",
+                columns: new[] { "ItemId", "SerialNumber", "StockKeyHash" },
+                unique: true,
+                filter: "[SerialNumber] IS NOT NULL");
+        }
+    }
+}
