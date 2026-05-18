6bfd337a9c307487a332f97e0bd9b3a9bda36852
Ali Majed HA <ali.majedha.business@gmail.com>
2026-05-18 14:40:16 +0330

    add receipt-source-of-supply dtos and mapping

---
+++ Src/WarehouseModule/Warehouse.Service/Mappings/ReceiptSourceOfSupplyMappingProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+
+using NGErp.Warehouse.Domain.Entities;
+using NGErp.Warehouse.Service.DTOs;
+
+namespace NGErp.Warehouse.Service.Mappings;
+
+public class ReceiptSourceOfSupplyMappingProfile : Profile
+{
+    public ReceiptSourceOfSupplyMappingProfile()
+    {
+        CreateMap<ReceiptSourceOfSupply, ReceiptSourceOfSupplyDto>();
+        CreateMap<CreateReceiptSourceOfSupplyDto, ReceiptSourceOfSupply>();
+        CreateMap<PatchReceiptSourceOfSupplyDto, ReceiptSourceOfSupply>().ReverseMap();
+    }
+}
