3745ae0d526b531b244c5eb13edc323a22e2fec6
Ali Majed HA <149356324+alimajedha-business@users.noreply.github.com>
2026-05-19 15:41:24 +0330

    feat: add remittance line dtos

---
+++ Src/WarehouseModule/Warehouse.Service/DTOs/RemittanceLineDto.cs
@@ -0,0 +1,42 @@
+namespace NGErp.Warehouse.Service.DTOs;
+
+public record RemittanceLineDto(
+    Guid Id,
+    int RowNumber,
+    Guid ItemId,
+    Guid WarehouseLocationId,
+    decimal? Weight,
+    decimal? Volume,
+    Guid? PreferredMassUnitId,
+    Guid? PreferredVolumeUnitId,
+    decimal? UnitPrice,
+    decimal? TotalPrice,
+    string? BatchNumber,
+    string? SerialNumber,
+    DateTime? ExpiryDate,
+    string? Description,
+    IReadOnlyList<RemittanceLineMeasurementValueDto> RemittanceLineMeasurementValues,
+    IReadOnlyList<RemittanceLineAttributeValueDto> RemittanceLineAttributeValues,
+    IReadOnlyList<RemittanceFieldValueDto> RemittanceFieldValues
+);
+
+public class CreateRemittanceLineDto
+{
+    public int RowNumber { get; set; }
+    public Guid ItemId { get; set; }
+    public Guid WarehouseLocationId { get; set; }
+    public decimal? Weight { get; set; }
+    public decimal? Volume { get; set; }
+    public Guid? PreferredMassUnitId { get; set; }
+    public Guid? PreferredVolumeUnitId { get; set; }
+    public decimal? UnitPrice { get; set; }
+    public decimal? TotalPrice { get; set; }
+    public string? BatchNumber { get; set; }
+    public string? SerialNumber { get; set; }
+    public DateTime? ExpiryDate { get; set; }
+    public string? Description { get; set; }
+
+    public List<CreateRemittanceLineMeasurementValueDto> RemittanceLineMeasurementValues { get; set; } = [];
+    public List<CreateRemittanceLineAttributeValueDto> RemittanceLineAttributeValues { get; set; } = [];
+    public List<CreateRemittanceFieldValueDto> RemittanceFieldValues { get; set; } = [];
+}
