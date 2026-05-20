55533857821e308667810194cffac1c78761e9ff
Ali Majed HA <149356324+alimajedha-business@users.noreply.github.com>
2026-05-20 08:22:56 +0330

    feat: add remittance type field configuration dto validator

---
+++ Src/WarehouseModule/Warehouse.Service/RequestValidators/DtoValidators/RemittanceTypeFieldConfigurationValidator.cs
@@ -0,0 +1,42 @@
+using FluentValidation;
+
+using Microsoft.Extensions.Localization;
+
+using NGErp.Warehouse.Domain.Entities;
+using NGErp.Warehouse.Service.DTOs;
+using NGErp.Warehouse.Service.Resources;
+
+namespace NGErp.Warehouse.Service.RequestValidators.DtoValidators;
+
+public class CreateRemittanceTypeFieldConfigurationValidator :
+    AbstractValidator<CreateRemittanceTypeFieldConfigurationDto>
+{
+    private readonly IStringLocalizer<WarehouseResource> _localizer;
+
+    public CreateRemittanceTypeFieldConfigurationValidator(
+        IStringLocalizer<WarehouseResource> localizer
+    )
+    {
+        _localizer = localizer;
+
+        RuleFor(p => p.FieldDefinitionId)
+            .NotEmpty()
+            .WithMessage(
+                _localizer["RemittanceTypeFieldConfiguration.FieldDefinition.NotEmpty"].Value
+            );
+
+        RuleFor(p => p.Exists)
+            .NotNull()
+            .WithMessage(_localizer["RemittanceTypeFieldConfiguration.Exists.NotNull"].Value);
+
+        RuleFor(p => p.IsRequired)
+            .NotNull()
+            .WithMessage(_localizer["RemittanceTypeFieldConfiguration.IsRequired.NotNull"].Value);
+
+        RuleFor(p => p.Placement)
+            .NotNull()
+            .WithMessage(_localizer["RemittanceTypeFieldConfiguration.Placement.NotNull"].Value)
+            .Must(p => p is null || Enum.IsDefined(typeof(ReceiptConfiguredPlacement), p.Value))
+            .WithMessage(_localizer["RemittanceTypeFieldConfiguration.Placement.Invalid"].Value);
+    }
+}
