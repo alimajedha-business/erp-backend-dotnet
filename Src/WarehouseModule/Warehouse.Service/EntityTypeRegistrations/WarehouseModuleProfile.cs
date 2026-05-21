f221aaa40e3d5b39a24b19f7a4dee66918c4002c
Ali Majed HA <ali.majedha.business@gmail.com>
2026-05-21 11:25:45 +0330

    fix ordering and code duplication

--- Src/WarehouseModule/Warehouse.Service/EntityTypeRegistrations/WarehouseModuleProfile.cs
+++ Src/WarehouseModule/Warehouse.Service/EntityTypeRegistrations/WarehouseModuleProfile.cs
@@ -68,7 +68,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             NameFa = "مقدار شمارشی ویژگی",
             NameEn = "Attribute Enum Value",
             Code = "0022",
-            Ordering = 130,
+            Ordering = 121,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -88,7 +88,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
         yield return new EntityTypeDefinition
         {
             Key = EntityTypes.Category,
-            NameFa = "دسته بندی",
+            NameFa = "طبقه‌بندی کالا",
             NameEn = "Category",
             Code = "0019",
             Ordering = 100,
@@ -114,7 +114,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             NameFa = "قانون ویژگی دسته بندی",
             NameEn = "Category Attr Rule",
             Code = "0023",
-            Ordering = 140,
+            Ordering = 101,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -137,7 +137,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             NameFa = "محدودیت سطح دسته بندی",
             NameEn = "Category Level Constraint",
             Code = "0020",
-            Ordering = 110,
+            Ordering = 102,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -160,7 +160,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             NameFa = "کالا",
             NameEn = "Item",
             Code = "0009",
-            Ordering = 80,
+            Ordering = 71,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -183,7 +183,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             NameFa = "ویژگی کالا",
             NameEn = "Item Attribute",
             Code = "0010",
-            Ordering = 82,
+            Ordering = 72,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -229,7 +229,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             NameFa = "واحد اندازه‌گیری کالا",
             NameEn = "Item Unit Of Measurement",
             Code = "0011",
-            Ordering = 84,
+            Ordering = 73,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -252,7 +252,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             NameFa = "تبدیل واحد کالا",
             NameEn = "Item Unit Conversion",
             Code = "0012",
-            Ordering = 86,
+            Ordering = 74,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -275,7 +275,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             NameFa = "کالای انبار",
             NameEn = "Item Warehouse",
             Code = "0013",
-            Ordering = 87,
+            Ordering = 75,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -298,7 +298,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             NameFa = "محل کالای انبار",
             NameEn = "Item Warehouse Location",
             Code = "0014",
-            Ordering = 88,
+            Ordering = 76,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -343,8 +343,8 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             Key = EntityTypes.ReceiptFieldDefinition,
             NameFa = "تعریف ورودی رسید",
             NameEn = "Receipt Field Definition",
-            Code = "0024",
-            Ordering = 150,
+            Code = "0025",
+            Ordering = 151,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -366,8 +366,8 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             Key = EntityTypes.ReceiptFieldValue,
             NameFa = "مقدار ورودی رسید",
             NameEn = "Receipt Field Value",
-            Code = "0024",
-            Ordering = 150,
+            Code = "0026",
+            Ordering = 152,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -389,8 +389,8 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             Key = EntityTypes.ReceiptLine,
             NameFa = "ردیف‌ رسید",
             NameEn = "Receipt Line",
-            Code = "0024",
-            Ordering = 150,
+            Code = "0027",
+            Ordering = 153,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -412,8 +412,8 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             Key = EntityTypes.ReceiptLineAttributeValue,
             NameFa = "مشخصه ردیف رسید",
             NameEn = "Receipt Line Attribute Value",
-            Code = "0024",
-            Ordering = 150,
+            Code = "0028",
+            Ordering = 154,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -435,8 +435,8 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             Key = EntityTypes.ReceiptLineAttributeValue,
             NameFa = "مقدار مشخصه ردیف رسید",
             NameEn = "Receipt Line Attribute Value",
-            Code = "0024",
-            Ordering = 150,
+            Code = "0029",
+            Ordering = 155,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -458,8 +458,8 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             Key = EntityTypes.ReceiptLineMeasurementValue,
             NameFa = "مقدار واحد اندازه گیری ردیف رسید",
             NameEn = "Receipt Line Measurement Value",
-            Code = "0024",
-            Ordering = 150,
+            Code = "0030",
+            Ordering = 156,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -478,11 +478,11 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
 				
 		yield return new EntityTypeDefinition
         {
-            Key = EntityTypes.ReceiptLineMeasurementValue,
+            Key = EntityTypes.ReceiptSourceOfSupply,
             NameFa = "منبع تامین رسید",
             NameEn = "Receipt Source of Supply",
-            Code = "0024",
-            Ordering = 150,
+            Code = "0031",
+            Ordering = 157,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -504,8 +504,8 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             Key = EntityTypes.ReceiptType,
             NameFa = "نوع رسید",
             NameEn = "Receipt Type",
-            Code = "0024",
-            Ordering = 150,
+            Code = "0032",
+            Ordering = 158,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -527,8 +527,8 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             Key = EntityTypes.ReceiptTypeConfiguration,
             NameFa = "پیکربندی نوع رسید",
             NameEn = "Receipt Type Configuration",
-            Code = "0024",
-            Ordering = 150,
+            Code = "0033",
+            Ordering = 159,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -550,8 +550,8 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             Key = EntityTypes.ReceiptTypeFieldConfiguration,
             NameFa = "پیکربندی ورودی نوع رسید",
             NameEn = "Receipt Type Field Configuration",
-            Code = "0024",
-            Ordering = 150,
+            Code = "0034",
+            Ordering = 160,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -573,8 +573,8 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             Key = EntityTypes.RemittanceType,
             NameFa = "نوع حواله",
             NameEn = "Remittance Type",
-            Code = "0025",
-            Ordering = 160,
+            Code = "0035",
+            Ordering = 170,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -596,7 +596,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             Key = EntityTypes.SiUnit,
             NameFa = "واحد SI",
             NameEn = "SI Unit",
-            Code = "0028",
+            Code = "0036",
             Ordering = 190,
             Attributes = new EntityTypeAttributes
             {
@@ -666,7 +666,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             NameFa = "انبار",
             NameEn = "Warehouse",
             Code = "0003",
-            Ordering = 20,
+            Ordering = 21,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -689,7 +689,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             NameFa = "محل انبار",
             NameEn = "Warehouse Location",
             Code = "0004",
-            Ordering = 30,
+            Ordering = 22,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
@@ -712,7 +712,7 @@ public class WarehouseModuleProfile : IEntityTypeModuleProfile
             NameFa = "نوع انبار",
             NameEn = "Warehouse Type",
             Code = "0002",
-            Ordering = 10,
+            Ordering = 20,
             Attributes = new EntityTypeAttributes
             {
                 Readable = true,
