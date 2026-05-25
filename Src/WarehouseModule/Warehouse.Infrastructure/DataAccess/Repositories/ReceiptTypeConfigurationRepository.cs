d24485f3bb7644f7dd4dcb6b751309a9fb576153
Ali Majed HA <ali.majedha.business@gmail.com>
2026-05-25 11:33:00 +0330

    improve receipt-type-field-conf repo and dto

--- Src/WarehouseModule/Warehouse.Infrastructure/DataAccess/Repositories/ReceiptTypeConfigurationRepository.cs
+++ Src/WarehouseModule/Warehouse.Infrastructure/DataAccess/Repositories/ReceiptTypeConfigurationRepository.cs
@@ -1,11 +1,8 @@
 using System.Linq.Expressions;
 
-using System.Linq.Dynamic.Core;
-
 using Microsoft.EntityFrameworkCore;
 
 using NGErp.Base.Infrastructure.DataAccess;
-using NGErp.Base.Service.RequestFeatures;
 using NGErp.General.Infrastructure.DataAccess.Repositories;
 using NGErp.Warehouse.Domain.Entities;
 using NGErp.Warehouse.Service.Repository.Contracts;
@@ -23,36 +20,9 @@ public class ReceiptTypeConfigurationRepository(MainDbContext context) :
     )
     {
         var query = trackChanges ? _dbSet : _dbSet.AsNoTracking();
-        return WithIncludes(query).SingleOrDefaultAsync(predicate, ct);
-    }
-
-    public override IQueryable<ReceiptTypeConfiguration> FilterByQ(
-        Guid companyId,
-        RequestParameters requestParameters
-    )
-    {
-        return WithIncludes(_dbSet.AsNoTracking())
-            .Where(e => e.CompanyId == companyId)
-            .Filter(requestParameters);
-    }
-
-    public override IQueryable<ReceiptTypeConfiguration> GetFiltered(
-        Guid companyId,
-        RequestAdvancedFilters requestAdvancedFilters
-    )
-    {
-        return WithIncludes(_dbSet.AsNoTracking())
-            .Where(e => e.CompanyId == companyId)
-            .Filter(requestAdvancedFilters);
-    }
-
-    private static IQueryable<ReceiptTypeConfiguration> WithIncludes(
-        IQueryable<ReceiptTypeConfiguration> query
-    )
-    {
         return query
             .Include(e => e.ReceiptType)
-            .Include(e => e.FieldConfigurations)
-                .ThenInclude(e => e.FieldDefinition);
+            .Include(e => e.FieldConfigurations).ThenInclude(e => e.FieldDefinition)
+            .SingleOrDefaultAsync(predicate, ct);
     }
 }
